aae91264b0b783e84d132c045ce1f054a40d01d2
Keerty Verma <keertyverma1303@gmail.com>
2024-04-22 22:22:08 +0530

    Add configuration file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
+dotenv.config();
+import config from "config";
+
 import app from "./app";
 
-dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`App is listening on PORT - ${PORT}`);
+  console.log(`Node Env = ${process.env.NODE_ENV}`);
+  console.log(`App name = ${config.get("appName")}`);
 });
