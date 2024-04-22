3e9d04a8014c34fe1f98c3a1de6dceb7721a7a30
Keerty Verma <keertyverma1303@gmail.com>
2024-04-22 22:27:40 +0530

    Add custom logger on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 import config from "config";
 
+import logger from "./utils/logger";
 import app from "./app";
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`App is listening on PORT - ${PORT}`);
-  console.log(`Node Env = ${process.env.NODE_ENV}`);
-  console.log(`App name = ${config.get("appName")}`);
+  logger.info(`App is listening on PORT - ${PORT}`);
+  logger.debug(`Node Env = ${process.env.NODE_ENV}`);
+  logger.debug(`App name = ${config.get("appName")}`);
 });
