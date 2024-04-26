07290fe3f9e8841c827855bcc78aea6d7fa44a18
Keerty Verma <keertyverma1303@gmail.com>
2024-04-26 14:06:44 +0530

    Validate password against regex in signup form

--- client/src/lib/validation/index.ts
+++ client/src/lib/validation/index.ts
@@ -3,9 +3,10 @@ import * as z from "zod";
 export const SignupValidation = z.object({
   name: z.string().min(2, { message: "Name must be at least 2 characters." }),
   email: z.string().email(),
-  password: z
-    .string()
-    .min(8, { message: "Password must be at least 8 characters." }),
+  password: z.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/, {
+    message:
+      "Password must be between 8 to 20 characters and include at least one numeric digit, one lowercase letter, and one uppercase letter.",
+  }),
 });
 
 export const LoginValidation = z.object({
