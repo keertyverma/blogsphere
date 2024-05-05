21ba29b8efc965b916678f68969af7c52f364d4e
Keerty Verma <keertyverma1303@gmail.com>
2024-05-05 16:13:14 +0530

    Upload image file to cloudinary

--- server/src/controllers/upload.controller.ts
+++ server/src/controllers/upload.controller.ts
@@ -1,15 +1,35 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import Joi from "joi";
 import { APIResponse, APIStatus } from "../types/api-response";
 import { uploadSecurely } from "../utils/cloudinary";
+import BadRequestError from "../utils/errors/bad-request";
 import logger from "../utils/logger";
+
+const validate = (req: { data: string }) => {
+  // data -> data URI of file in base64 encoding string
+  const schema = Joi.object({
+    data: Joi.string()
+      .required()
+      .pattern(/^data:(image\/\w+);base64,([A-Za-z0-9+/=]+)/)
+      .message("Invalid data URI format. It must be encoded in base64"),
+  });
+
+  return schema.validate(req);
+};
 
 export const uploadSingleImage = async (req: Request, res: Response) => {
   logger.debug(`POST Request on Route -> ${req.baseUrl}`);
   // validate request body
+  const { error } = validate(req.body);
+  if (error) {
+    let errorMessage = error.details[0].message;
+    logger.error(`Input Validation Error! \n ${errorMessage}`);
+    throw new BadRequestError(errorMessage);
+  }
 
-  const uploadedImgURL = await uploadSecurely("");
-
+  const { data } = req.body;
+  const uploadedImgURL = await uploadSecurely(data);
   const result: APIResponse = {
     status: APIStatus.SUCCESS,
     statusCode: StatusCodes.CREATED,
@@ -17,5 +37,6 @@ export const uploadSingleImage = async (req: Request, res: Response) => {
       url: uploadedImgURL,
     },
   };
+
   return res.status(result.statusCode).json(result);
 };
--- server/src/utils/cloudinary.ts
+++ server/src/utils/cloudinary.ts
@@ -1,5 +1,11 @@
+import {
+  v2 as cloudinary,
+  UploadApiOptions,
+  UploadApiErrorResponse,
+} from "cloudinary";
+import config from "config";
 import "dotenv/config";
-import { v2 as cloudinary, UploadApiOptions } from "cloudinary";
+import BadRequestError from "./errors/bad-request";
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -7,13 +13,20 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export const uploadSecurely = async (fileData: any) => {
-  const options: UploadApiOptions = {
-    signed: true,
-  };
+export const uploadSecurely = async (data: string) => {
+  // upload file to cloudinary by using data URI of file in base64 encoding
+  // return secure url
+  try {
+    const options: UploadApiOptions = {
+      signed: true,
+      folder: config.get("appName"),
+    };
 
-  const result = await cloudinary.uploader.upload(fileData, options);
-  console.log(result);
-
-  return result.secure_url;
+    const result = await cloudinary.uploader.upload(data, options);
+    return result.secure_url;
+  } catch (error) {
+    const err = error as UploadApiErrorResponse;
+    if (err.http_code === 400) throw new BadRequestError("Invalid image file");
+    throw err;
+  }
 };
