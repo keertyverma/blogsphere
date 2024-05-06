1dc0a18ec643373ac4c5ebd39b8c29c274b7df89
Keerty Verma <keertyverma1303@gmail.com>
2024-05-06 21:39:43 +0530

    Customize editor js to upload image by file and by url from text editor

--- client/src/lib/editorjs-tools.ts
+++ client/src/lib/editorjs-tools.ts
@@ -5,6 +5,56 @@ import InlineCode from "@editorjs/inline-code";
 import List from "@editorjs/list";
 import Marker from "@editorjs/marker";
 import Quote from "@editorjs/quote";
+import { fileToBase64 } from "./utils";
+import apiClient from "./api-client";
+
+const uploadImageByURL = async (e: string) => {
+  let imageURL = null;
+
+  const link = new Promise((resolve, reject) => {
+    try {
+      resolve(e);
+    } catch (error) {
+      reject(error);
+    }
+  });
+
+  imageURL = await link;
+  if (imageURL) {
+    return { success: 1, file: { url: imageURL } };
+  }
+
+  return;
+};
+
+const uploadImage = async (img: File) => {
+  let imgURL = null;
+
+  // get base64 image string
+  const base64EncodedImg = await fileToBase64(img);
+  try {
+    const res = await apiClient
+      .post("/upload", { data: base64EncodedImg })
+      .then((res) => res.data);
+
+    imgURL = res.data.url;
+  } catch (error) {
+    console.error("Error in uploading file from editor. ", error);
+  }
+
+  return imgURL;
+};
+
+const uploadImageByFile = async (e: File) => {
+  const url = await uploadImage(e);
+  if (url) {
+    return {
+      success: 1,
+      file: { url },
+    };
+  }
+  return;
+};
 
 export const editorJSTools = {
   header: {
@@ -19,8 +69,19 @@ export const editorJSTools = {
     class: List,
     inlineToolbar: true,
   },
-  image: Image,
-  quote: Quote,
+  image: {
+    class: Image,
+    config: {
+      uploader: {
+        uploadByUrl: uploadImageByURL,
+        uploadByFile: uploadImageByFile,
+      },
+    },
+  },
+  quote: {
+    class: Quote,
+    inlineToolbar: true,
+  },
   marker: Marker,
   inlineCode: InlineCode,
   embed: Embed,
