0a395fb1b255377f5b42f2938ac4f7775ad8606e
Keerty Verma <keertyverma1303@gmail.com>
2024-05-20 15:36:26 +0530

    Add horizondal scroll indicator for filter by tag list

--- client/src/components/home/TagList.tsx
+++ client/src/components/home/TagList.tsx
@@ -1,6 +1,12 @@
+import { useEffect, useRef, useState } from "react";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 import { Button } from "../ui/button";
 
 const TagList = () => {
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+  const [showLeftArrow, setShowLeftArrow] = useState(false);
+  const [showRightArrow, setShowRightArrow] = useState(true);
+
   const categories = [
     "All",
     "science",
@@ -16,9 +22,58 @@ const TagList = () => {
     "health",
   ];
 
+  const handleScroll = (direction: "left" | "right") => {
+    if (scrollContainerRef.current) {
+      const { scrollLeft, clientWidth } = scrollContainerRef.current;
+      const scrollAmount = clientWidth / 2; // Adjust scroll amount if necessary
+      const newScrollPosition =
+        direction === "left"
+          ? scrollLeft - scrollAmount
+          : scrollLeft + scrollAmount;
+      scrollContainerRef.current.scrollTo({
+        left: newScrollPosition,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const updateArrowsVisibility = () => {
+    if (scrollContainerRef.current) {
+      const { scrollLeft, clientWidth, scrollWidth } =
+        scrollContainerRef.current;
+      setShowLeftArrow(scrollLeft > 0);
+      setShowRightArrow(scrollLeft + clientWidth < scrollWidth);
+    }
+  };
+
+  useEffect(() => {
+    updateArrowsVisibility();
+    const scrollContainer = scrollContainerRef.current;
+    if (scrollContainer) {
+      scrollContainer.addEventListener("scroll", updateArrowsVisibility);
+      return () => {
+        scrollContainer.removeEventListener("scroll", updateArrowsVisibility);
+      };
+    }
+  }, []);
+
   return (
-    <div className="w-full max-w-[90%] md:max-w-[800px] p-0 md:mt-4 mb-4 overflow-x-auto text-nowrap">
-      <div className="flex gap-1">
+    <div className="relative w-full max-w-[90%] md:max-w-[800px] p-0 md:mt-4 mb-4">
+      {showLeftArrow && (
+        <Button
+          variant={"secondary"}
+          size={"icon"}
+          className="max-md:hidden absolute left-0 top-0 bottom-0 z-10 flex-center bg-white hover:bg-gray-200"
+          onClick={() => handleScroll("left")}
+        >
+          <IoChevronBackOutline className="text-lg text-gray-500" />
+        </Button>
+      )}
+
+      <div
+        ref={scrollContainerRef}
+        className="flex gap-1 text-nowrap overflow-x-auto md:overflow-x-hidden md:overflow-y-hidden scroll-smooth"
+      >
         {categories.map((category, index) => (
           <Button
             variant="secondary"
@@ -30,6 +85,16 @@ const TagList = () => {
           </Button>
         ))}
       </div>
+      {showRightArrow && (
+        <Button
+          variant={"secondary"}
+          size={"icon"}
+          className="max-md:hidden absolute right-0 top-0 bottom-0 z-10 flex-center bg-white hover:bg-gray-200"
+          onClick={() => handleScroll("right")}
+        >
+          <IoChevronForwardOutline className="text-lg text-gray-500" />
+        </Button>
+      )}
     </div>
   );
 };
