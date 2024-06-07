f19279cffb6d8ebf310a1829625582ee811fbc92
Keerty Verma <keertyverma1303@gmail.com>
2024-06-08 01:35:49 +0530

    Update Editor JS Quotes display

--- client/src/components/blog/BlockQuote.tsx
+++ client/src/components/blog/BlockQuote.tsx
@@ -6,9 +6,11 @@ interface Props {
 const BlockQuote = ({ quote, caption }: Props) => {
   return (
     <div className="bg-primary/20 p-3 pl-5 border-l-4 border-primary">
-      <p className="!text-base lg:!text-lg !leading-6">{quote}</p>
+      <blockquote className="!text-base lg:!text-lg !leading-6">
+        &ldquo;{quote}&rdquo;
+      </blockquote>
       {caption && caption.length > 0 && (
-        <p className="w-full !text-sm md:!text-base mt-2 text-primary">
+        <p className="w-full !text-sm md:!text-base mt-2 text-muted-foreground text-right">
           {caption}
         </p>
       )}
