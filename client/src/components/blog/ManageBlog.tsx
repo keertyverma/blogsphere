892370ace6b7eea0fbe2037d5a6719b6c37fec64
Keerty Verma <keertyverma1303@gmail.com>
2024-06-19 17:28:02 +0530

    Add alert dialog box for blog delete confirmation

--- client/src/components/blog/ManageBlog.tsx
+++ client/src/components/blog/ManageBlog.tsx
@@ -1,6 +1,17 @@
 import { MdEdit, MdOutlineDelete } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
 
 interface Props {
   blogId: string;
@@ -20,13 +31,30 @@ const ManageBlog = ({ blogId }: Props) => {
         <MdEdit className="text-lg text-muted-foreground" />
       </Button>
 
-      <Button
-        variant="outline"
-        size="icon"
-        className="w-8 h-8 rounded-full flex-center"
-      >
-        <MdOutlineDelete className="text-lg text-destructive" />
-      </Button>
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <div className="w-8 h-8 rounded-full flex-center border bg-background hover:bg-accent hover:text-accent-foreground">
+            <MdOutlineDelete className="text-lg text-destructive" />
+          </div>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Delete Confirmation</AlertDialogTitle>
+            <AlertDialogDescription className="text-secondary-foreground">
+              Are you absolutely sure? This will permanently delete your blog.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+              onClick={() => console.log("handle delete")}
+            >
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
