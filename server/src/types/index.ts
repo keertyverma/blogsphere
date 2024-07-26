787139058bebce35195d362c6775008f7513a22b
Keerty Verma <keertyverma1303@gmail.com>
2024-07-26 23:38:08 +0530

    add email sending functionality using Mailgun

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -3,3 +3,10 @@ type SortOrderType = 1 | -1;
 export interface SortQuery {
   [key: string]: SortOrderType;
 }
+
+export interface MailOptions {
+  to: string;
+  subject: string;
+  text: string;
+  html?: string;
+}
