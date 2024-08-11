1ddd9c4441d65c96d0d03d513a899787bb67ed45
Keerty Verma <keertyverma1303@gmail.com>
2024-08-12 00:02:53 +0530

    Fix: Link inside quotes caption is not displayed properly

--- client/src/components/blog/BlockQuote.tsx
+++ client/src/components/blog/BlockQuote.tsx
@@ -1,18 +1,23 @@
+import DOMPurify from "dompurify";
+
 interface Props {
   quote: string;
   caption?: string;
 }
 
 const BlockQuote = ({ quote, caption }: Props) => {
+  if (!quote) return null;
+
   return (
     <div className="bg-primary/20 p-3 pl-5 border-l-4 border-primary rounded-md">
       <blockquote className="!text-base lg:!text-lg !leading-6">
         &ldquo;{quote}&rdquo;
       </blockquote>
       {caption && caption.length > 0 && (
-        <p className="w-full !text-sm md:!text-base mt-2 text-muted-foreground text-right">
-          {caption}
-        </p>
+        <p
+          className="quote-caption w-full !text-sm md:!text-base mt-2 text-muted-foreground text-right"
+          dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(caption) }}
+        ></p>
       )}
     </div>
   );
