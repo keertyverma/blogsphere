081164110cd79b60aa58d288fc8aa175389bfee8
Keerty Verma <keertyverma1303@gmail.com>
2025-01-22 11:39:49 +0530

    Make trending blog list sticky to the top of the right-side navigation

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -27,9 +27,9 @@ const Home = () => {
               <GetLatestBlogs />
             </div>
 
-            <aside className="min-w-[40%] lg:min-w-[400px] max-w-min max-md:hidden border-l border-border pl-8 pt-3 mt-4">
+            <aside className="relative min-w-[40%] lg:min-w-[400px] max-w-min max-md:hidden border-l border-border pl-8 pt-3 mt-4">
               {/* trending blogs */}
-              <div>
+              <div className="sticky -top-[15%]">
                 <h1 className="font-semibold text-xl mb-4 flex gap-2">
                   Trending
                   <IoTrendingUpSharp className="text-muted-foreground" />
