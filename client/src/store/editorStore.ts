62179de40a203d9bbdf58fabbf65998f62f4bd35
Keerty Verma <keertyverma1303@gmail.com>
2025-02-14 18:21:43 +0530

    feat:Check for unsaved changes before preview and notify user to save manually to prevent stale data and loss of changes.

--- client/src/store/editorStore.ts
+++ client/src/store/editorStore.ts
@@ -10,12 +10,14 @@ export const INITIAL_BLOG = {
   description: "",
   authorDetails: {} as IAuthor,
   tags: [],
-  content: {} as OutputData,
+  content: { blocks: [] } as OutputData,
 };
 
 interface EditorStore {
-  blog: IBlog;
+  blog: IBlog; // the current blog being edited
+  lastSavedBlog: IBlog; // the last version of the blog saved in the db
   setBlog: (blog: IBlog) => void;
+  setLastSavedBlog: (blog: IBlog) => void;
   isPublish: boolean;
   setIsPublish: (isPublish: boolean) => void;
   textEditor: EditorJS | null;
@@ -31,11 +33,13 @@ export const useEditorStore = create<EditorStore>()(
   persist(
     (set) => ({
       blog: INITIAL_BLOG,
+      lastSavedBlog: INITIAL_BLOG,
       isPublish: false,
       textEditor: null,
       isPublishClose: false,
       selectedTag: "all",
       setBlog: (blog) => set({ blog }),
+      setLastSavedBlog: (blog) => set({ lastSavedBlog: blog }),
       setIsPublish: (isPublish) => set({ isPublish }),
       setTextEditor: (textEditor) => set({ textEditor }),
       setIsPublishClose: (isPublishClose) => set({ isPublishClose }),
