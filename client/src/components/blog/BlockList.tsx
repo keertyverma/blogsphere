d958474898ca61537efded44e14ea2a92aeedec1
Keerty Verma <keertyverma1303@gmail.com>
2025-03-03 00:24:29 +0530

    feat: Add support for EditorJS checklist

--- client/src/components/blog/BlockList.tsx
+++ client/src/components/blog/BlockList.tsx
@@ -1,12 +1,18 @@
 import { sanitizeContent } from "@/lib/utils";
+import { MdCheckBoxOutlineBlank } from "react-icons/md";
+
+interface Meta {
+  checked?: boolean;
+}
 
 interface ListItem {
   content: string;
+  meta?: Meta;
   items: ListItem[];
 }
 
 interface Props {
-  style: "ordered" | "unordered";
+  style: "ordered" | "unordered" | "checklist";
   items: ListItem[];
 }
 
@@ -20,10 +26,23 @@ const BlockList = ({ style, items }: Props) => {
       // Compute hierarchical index for ordered lists
       const index = parentIndex ? `${parentIndex}.${i + 1}` : `${i + 1}`;
       const safeHTML = sanitizeContent(item.content);
+      const isChecked = item.meta?.checked || false;
 
       return (
         <li key={index} className="my-2">
-          {style === "ordered" && <span className="mr-2">{index}.</span>}
+          {style === "ordered" && level > 0 && (
+            <span className="mr-1 md:mr-2">{`${index}.`}</span>
+          )}
+          {style === "checklist" && (
+            <span className={"mr-1 md:mr-2"}>
+              {isChecked ? (
+                "✅"
+              ) : (
+                <MdCheckBoxOutlineBlank className="inline text-muted-foreground h-5 w-5 md:h-6 md:w-6" />
+              )}
+            </span>
+          )}
+
           <span dangerouslySetInnerHTML={{ __html: safeHTML }}></span>
 
           {/* Recursively render nested lists if items exist */}
@@ -43,21 +62,30 @@ const BlockList = ({ style, items }: Props) => {
   );
 };
 
-// Helper component to dynamically choose list type
 const ListTag = ({
   style,
   children,
   level,
 }: {
-  style: "ordered" | "unordered";
+  style: "ordered" | "unordered" | "checklist";
   children: React.ReactNode;
   level: number;
 }) => {
-  return style === "ordered" ? (
-    <ol className={`${level > 0 ? "pl-5" : ""}`}>{children}</ol>
-  ) : (
-    <ul className="pl-5 list-disc">{children}</ul>
-  );
+  if (style === "ordered") {
+    return (
+      <ol className={`${level === 0 ? "pl-5 list-decimal" : ""}`}>
+        {children}
+      </ol>
+    );
+  } else if (style === "unordered") {
+    return <ul className="pl-4 md:pl-5 list-disc">{children}</ul>;
+  } else {
+    return (
+      <ul className={`${level > 0 ? "pl-6 md:pl-8" : ""} list-none`}>
+        {children}
+      </ul>
+    );
+  }
 };
 
 export default BlockList;
