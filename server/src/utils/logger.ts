9c8ec8acd08456dc15fa9fb7c103a6c15918396b
Keerty Verma <keertyverma1303@gmail.com>
2025-03-05 16:04:59 +0530

    updated for production node env variable

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -2,23 +2,23 @@ import fs from "fs";
 import path from "path";
 import { Logger, createLogger, transports, LoggerOptions } from "winston";
 
-// check if logs directory exists, if not create it
-const logDir = path.join(__dirname, "../../logs");
-try {
-  if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
-} catch (error) {
-  console.error("Failed to create log directory:", error);
-}
 const loggerOptions: LoggerOptions = {
   level: process.env.LOG_LEVEL?.toLowerCase() === "debug" ? "debug" : "info",
   transports: [new transports.Console()],
-  exceptionHandlers: [
-    new transports.File({ filename: path.join(logDir, "exceptions.log") }),
-  ],
 };
 
-if (process.env.NODE_ENV === "production") {
-  delete loggerOptions.exceptionHandlers;
+if (process.env.NODE_ENV !== "production") {
+  // check if logs directory exists, if not create it
+  const logDir = path.join(__dirname, "../../logs");
+  try {
+    if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
+  } catch (error) {
+    console.error("Failed to create log directory:", error);
+  }
+
+  loggerOptions.exceptionHandlers = [
+    new transports.File({ filename: path.join(logDir, "exceptions.log") }),
+  ];
 }
 
 const logger: Logger = createLogger(loggerOptions);
