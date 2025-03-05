513705418e6f260e71f2a0e19c79422cc177be02
Keerty Verma <keertyverma1303@gmail.com>
2025-03-05 15:41:19 +0530

    logger changes to remove exception file from prod

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { Logger, createLogger, transports } from "winston";
+import { Logger, createLogger, transports, LoggerOptions } from "winston";
 
 // check if logs directory exists, if not create it
 const logDir = path.join(__dirname, "../../logs");
@@ -9,13 +9,18 @@ try {
 } catch (error) {
   console.error("Failed to create log directory:", error);
 }
-
-const logger: Logger = createLogger({
+const loggerOptions: LoggerOptions = {
   level: process.env.LOG_LEVEL?.toLowerCase() === "debug" ? "debug" : "info",
   transports: [new transports.Console()],
   exceptionHandlers: [
     new transports.File({ filename: path.join(logDir, "exceptions.log") }),
   ],
-});
+};
+
+if (process.env.NODE_ENV === "production") {
+  delete loggerOptions.exceptionHandlers;
+}
+
+const logger: Logger = createLogger(loggerOptions);
 
 export default logger;
