4e14cf13ef035f1f62c728abab7868672c138df9
Keerty Verma <keertyverma1303@gmail.com>
2025-03-18 12:40:05 +0530

    Add CI/CD pipeline for backend API using GitHub Actions and deploy to Google App Engine (#8)

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,21 +1,26 @@
 import fs from "fs";
 import path from "path";
-import { Logger, createLogger, transports } from "winston";
+import { Logger, createLogger, transports, LoggerOptions } from "winston";
 
-// check if logs directory exists, if not create it
-const logDir = path.join(__dirname, "../../logs");
-try {
-  if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
-} catch (error) {
-  console.error("Failed to create log directory:", error);
-}
-
-const logger: Logger = createLogger({
+const loggerOptions: LoggerOptions = {
   level: process.env.LOG_LEVEL?.toLowerCase() === "debug" ? "debug" : "info",
   transports: [new transports.Console()],
-  exceptionHandlers: [
+};
+
+if (process.env.NODE_ENV !== "production") {
+  // check if logs directory exists, if not create it
+  const logDir = path.join(__dirname, "../../logs");
+  try {
+    if (!fs.existsSync(logDir)) fs.mkdirSync(logDir, { recursive: true });
+  } catch (error) {
+    console.error("Failed to create log directory:", error);
+  }
+
+  loggerOptions.exceptionHandlers = [
     new transports.File({ filename: path.join(logDir, "exceptions.log") }),
-  ],
-});
+  ];
+}
+
+const logger: Logger = createLogger(loggerOptions);
 
 export default logger;
